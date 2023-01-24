37598d12f889bb3d50b6795d4c7ce4054beb930a
SURYA AGUNG ELEKTRONIK <m.taufiqtaufiq744@gmail.com>
2023-01-24 09:58:27 +0700

    edit loginn

--- forms/register.php
+++ forms/register.php
@@ -5,7 +5,7 @@
 	<meta name="author" content="Kodinger">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
     <link href="../assets/img/logo.png" rel="icon">
-	<title>My Login Page &mdash; Bootstrap 4 Login Page Snippet</title>
+	<title>Register Page &mdash; Bootstrap 4 Login Page Snippet</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="../assets/css/my-login.css">
 </head>
@@ -40,8 +40,7 @@
 
 								<div class="form-group">
 									<label for="password"class="form-label">PASSOWRD</label>
-									<input id="password" type="password" class="form-control" name="password"  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                      aria-describedby="password"  required data-eye>
+									<input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" required data-eye/>
 									<div class="invalid-feedback">
 										Password is required
 									</div>
@@ -78,6 +77,6 @@
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="js/my-login.js"></script>
+	<script src="../assets/js/my-login.js"></script>
 </body>
 </html>

1d81c25f576db5e0a154ac786ac4d07db85bec9b
SURYA AGUNG ELEKTRONIK <m.taufiqtaufiq744@gmail.com>
2023-02-02 09:02:45 +0700

    mengganti text justify

--- tentang/sejarah.php
+++ tentang/sejarah.php
@@ -87,7 +87,7 @@
                     <div class="icon-box">
                         <!-- <div class="icon"><i class="bx bx-fingerprint"></i></div> -->
                         <h4 class="title"><a href="">Sejarah SMK</a></h4>
-                        <p class="description text-justify">SMK Negeri 1 Pasuruan bermula dari Sekolah Menengah Ekonomi Pertama (SMEP) yang berdomisili di Jalan Veteran 11 Pasuruan, Telp. 0343-421380, dibangun diatas tanah seluas 8950 meter persegi berdasarkan sertifikat No. 886/ 1985. Pada tahun 1977 dengan SK No 0278/U/1976 dari SMEP diintegrasikan menjadi SMP yang disempurnakan. Pada Tahun pelajaran baru 1978 ditingkatkan menjadi SLTA Kejuruan (SMEA Negeri) Pasuruan dengan lampiran Keputusan Menteri Pendidikan dan Kebudayaan tanggal 30 Juli 1980 No. 0209/0/1980. Pada Tahun 2000 SMEA Negeri Pasuruan diubah nama menjadi SMK Negeri 1 Pasuruan Kelompok Bisnis dan Manajemen dan Kelompok Teknologi.</p>
+                        <p class="description">SMK Negeri 1 Pasuruan bermula dari Sekolah Menengah Ekonomi Pertama (SMEP) yang berdomisili di Jalan Veteran 11 Pasuruan, Telp. 0343-421380, dibangun diatas tanah seluas 8950 meter persegi berdasarkan sertifikat No. 886/ 1985. Pada tahun 1977 dengan SK No 0278/U/1976 dari SMEP diintegrasikan menjadi SMP yang disempurnakan. Pada Tahun pelajaran baru 1978 ditingkatkan menjadi SLTA Kejuruan (SMEA Negeri) Pasuruan dengan lampiran Keputusan Menteri Pendidikan dan Kebudayaan tanggal 30 Juli 1980 No. 0209/0/1980. Pada Tahun 2000 SMEA Negeri Pasuruan diubah nama menjadi SMK Negeri 1 Pasuruan Kelompok Bisnis dan Manajemen dan Kelompok Teknologi.</p>
                     </div>
 
                     <!-- <div class="icon-box">
